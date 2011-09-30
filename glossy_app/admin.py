b4feaf4502efff83c9b36193cdf243a8635f00c3
Simon Vansintjan <svansintjan@gmail.com>
2011-09-30 12:06:29 +0100

    added admin.py file

---
+++ glossy_app/admin.py
@@ -0,0 +1,10 @@
+from polls.models import * 
+from django.contrib import admin
+
+admin.site.register(Language)
+admin.site.register(Word)
+admin.site.register(Language_version)
+admin.site.register(Definition)
+admin.site.register(Comment)
+
+
